2703e0d21794ab0c6ed29de811db9decaa3910d4
Bruce Masiko <bruce.masiko@student.moringaschool.com>
2023-03-16 18:32:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
-const currentUser = "Bruce Masiko"
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+const currentUser = "Bruce Masiko";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
 
 
